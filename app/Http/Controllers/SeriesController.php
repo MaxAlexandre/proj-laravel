8222b72b0e55d291190b2636416ae35be25e5b3c
Max Ferreira <max.ferreira@unisuam.edu.br>
2020-02-19 15:39:40 -0300

    Finalizando primeira parte do projeto

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,22 +4,39 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\SeriesFormRequest;
+use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request) {
-        $series = [
-            'Game of Thrones',
-            'Rick an Morty',
-            'Naruto'
-        ];
-        $html = "<ul>";
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= "</ul>";
+    public function index(Request $request)
+    {
+        $series = Serie::query()->orderBy('nome')->get();
 
-        return $html;
+        $mensagem = $request->session()->get('mensagem');
+
+        return view('series.index', compact('series','mensagem'));
     }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = Serie::create($request->all());
+        $request->session()->flash('mensagem',"Série {$serie->id} criada com sucesso {$serie->nome}");
+
+        return redirect()->route('listar_series');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->id);
+        $request->session()->flash('mensagem',"Série removida com sucesso!");
+        return redirect()->route('listar_series');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,7 @@
 |
 */
 
-Route::get('/series', 'SeriesController@index');
+Route::get('/series', 'SeriesController@index')->name('listar_series');
+Route::get('/series/criar', 'SeriesController@create')->name('form_criar_serie');
+Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
