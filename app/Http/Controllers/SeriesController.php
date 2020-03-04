a2a16ade59d5154eb1c34e68d8afe6bd7ec2f62c
Max Ferreira <max.ferreira@unisuam.edu.br>
2020-03-04 17:31:33 -0300

    Excluir e editar séries

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
+use App\Services\CriadorDeSerie;
+use App\Services\RemovedorDeSeries;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -24,28 +26,34 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(SeriesFormRequest $request)
+    public function store(SeriesFormRequest $request, CriadorDeSerie $criadorDeSerie)
     {
-        $serie = Serie::create(['nome' => $request->nome]);
-        $qtdTemporadas = $request->qtd_temporadas;
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for ($j = 1; $j <= $request->ep_por_temporada; $j++  ) {
-                 $temporada->episodios()->create(['numero' => $i]);
-            }
-        }
+        $serie = $criadorDeSerie->criarSerie(
+            $request->nome,
+            $request->qtd_temporadas,
+            $request->ep_por_temporada
+        );
 
         $request->session()->flash('mensagem', "Série {$serie->id} e suas temporadas e episódios criadas com sucesso {$serie->nome}");
 
         return redirect()->route('listar_series');
     }
 
-    public function destroy(Request $request)
+    public function destroy(Request $request, RemovedorDeSeries $removedorDeSeries)
     {
-        Serie::destroy($request->id);
-        $request->session()->flash('mensagem', "Série removida com sucesso!");
+        $nomeSerie = $removedorDeSeries->removerSerie($request->id);
+
+        $request->session()->flash('mensagem', "Série $nomeSerie removida com sucesso!");
         return redirect()->route('listar_series');
+    }
+
+    public function editaNome(int $id, Request $request)
+    {
+        $novoNome = $request->nome;
+        $serie = Serie::find($id);
+        $serie->nome = $novoNome;
+        $serie->save();
     }
 
 }
